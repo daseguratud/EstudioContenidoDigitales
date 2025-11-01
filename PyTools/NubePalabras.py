fc944715706ea494dadafddfc05ff9d18b29eb54
Dario Alejandro Segura Torres <dariosegura@usantotomas.edu.co>
2025-11-01 10:33:05 -0500

    Almacena imagen y frecuencias del Cloud word

--- PyTools/NubePalabras.py
+++ PyTools/NubePalabras.py
@@ -10,11 +10,16 @@ class NubePalabras:
             width=800, height=400, 
             background_color='white', 
             colormap='viridis').generate(texto)
-        frequencies = nube_palabras.words_        
-        plt.figure(figsize=(10, 5))
-        plt.imshow(nube_palabras, interpolation='bilinear')
-        plt.axis('off')
-        plt.show()
+        frequencies = nube_palabras.words_
+        nube_palabras.to_file(Parameters.getOutFileNubePalabrasImg())
+        # plt.figure(figsize=(10, 5))
+        # plt.imshow(nube_palabras, interpolation='bilinear')
+        # plt.axis('off')
+        # plt.show()
+        datFrec=[]
+        for frecuency in frequencies.items():
+            data=f"{frecuency[0]},{frecuency[1]}"
+            datFrec.append(data)
         Parameters.CreatePath(Parameters.nubePalabrasPath)
         Parameters.CreateFile(Parameters.getOutFileNubePalabrasFrec())
-        Parameters.WriteAllText(Parameters.getOutFileNubePalabrasFrec(),str.join("\n",frequencies))
+        Parameters.WriteAllText(Parameters.getOutFileNubePalabrasFrec(),str.join("\n",datFrec))
